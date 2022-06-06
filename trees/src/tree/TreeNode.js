16b9920462cd585df2ca7db994fda181f9e3c2b2
jsvanilla <36719905+jsvanilla@users.noreply.github.com>
2022-06-06 01:44:27 -0500

    [FEATURE] NEXT bonus2

--- trees/src/tree/TreeNode.js
+++ trees/src/tree/TreeNode.js
@@ -3,9 +3,10 @@ import "./index.css";
 
 const TreeNode = ({ node, children, numberNameprop }) => {
   let numberName = numberNameprop ? numberNameprop : '1'
+  let nodeRender = `${node.substring(0,1)}${numberName.replace(/\d/g,'')}${node.substring(1,node.length)}`
   return (
     <div className="node">
-      <div>{`${numberName}  ${node}`}</div>
+      <div>{`${numberName}  ${nodeRender}`}</div>
       {children &&
         children.map((node, index) => {
           node.numberNameprop = `${numberName}.${index+1}`
